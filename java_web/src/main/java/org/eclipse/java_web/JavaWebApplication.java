384e3e5c4940fd3d9c230dcc9867c9314db7b5d9
Lunar <nightsky5070@gmail.com>
2025-06-26 10:18:07 +0900

    feat: initial commit

---
+++ java_web/src/main/java/org/eclipse/java_web/JavaWebApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse.java_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebApplication.class, args);
+    }
+
+}
